f7219ad743861501c5e7ff7ea0de2bc709468b44
mfirmanakbar <em.firman.akbar@gmail.com>
2023-03-31 05:17:54 +0700

    multiple component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css'
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import './App.css';
 
 export default function App() {
   const username = "firman";
   return (
+    <>
+    <Header/>
     <div>
       <h1 className='active'>{username}</h1>
       <p>Lorem ipsum dolor, placeat.</p>
     </div>
+    <Footer/>
+    </>
   )
 }
