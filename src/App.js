671c0435db5e5e42b730fe24d142d5005c5650b2
mfirmanakbar <em.firman.akbar@gmail.com>
2023-03-31 05:04:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
 
 export default function App() {
+  const username = "firman";
   return (
-    <div>App</div>
+    <div>
+      <h1 className='active'>{username}</h1>
+      <p>Lorem ipsum dolor, placeat.</p>
+    </div>
   )
 }
