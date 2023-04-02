b827984e71edc3f9d73abccf25b449f76e018753
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-02 20:13:52 +0700

    use state basic counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const username = "firman";
+  const [count, setCount] = useState(0);
+
+  function handleAdd() {
+    setCount(count + 1);
+  }
+
+  function handleSub() {
+    setCount(count - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
-    <>
-    <Header/>
-    <div>
-      <h1>{username}</h1>
-      <p className="active">Hello</p>
-      </div>
-    <Footer/>
-    </>
+   <div className='App'>
+    <div className='box'>
+      <p>{count}</p>
+      
+      <button onClick={handleAdd} className='add'>ADD</button>
+      <button onClick={handleSub} className='sub'>SUB</button>
+      <button onClick={handleReset} className='reset'>RESET</button>
+
+      {/* or we also can use anonymous function */}
+      {/* <button onClick={() => setCount(count + 1)} className='add'>ADD</button>
+      <button onClick={() => setCount(count - 1)} className='sub'>SUB</button>
+      <button onClick={() => setCount(0)} className='reset'>RESET</button> */}
+
+    </div>
+   </div>
   )
 }
