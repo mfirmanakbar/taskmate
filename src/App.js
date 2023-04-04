d1402ea5af596ed188300344b3492a3c56543e98
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-04 07:33:44 +0700

    add task

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Header } from './components/Header';
 import { TaskList } from './components/TaskList';
 import { Footer } from './components/Footer';
 import './App.css';
+import { AddTask } from './components/AddTask';
 
 function App() {
   const info = "Random";
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AddTask />
       <TaskList title='Random' info={info}/>
       <Footer />
     </div>
