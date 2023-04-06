209c653a08db81ffbfbbf76ccc09e3044be698a6
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-06 20:07:12 +0700

    add task global di app.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,19 +3,11 @@ import { TaskCard } from './TaskCard';
 import { BoxCard } from './BoxCard';
 import "./TaskList.css";
 
-export const TaskList = ({title, info, tasks, setTasks}) => { // bisa juga langsung => export const TaskList = ({title, info})
-    // const [tasks, setTasks] = useState(
-    //     [
-    //         {id: 5271, name: "Record React Lectures", completed: true},
-    //         {id: 7825, name: "Edit React Lectures", completed: false},
-    //         {id: 8391, name: "Warch Lectures", completed: false}
-    //     ]
-    // );
+export const TaskList = ({title, info, tasks, setTasks}) => {
     const [show, setShow] = useState(true);
 
     function handleDelete(id) {
-        // we remove it by filter and the useState will be changed
-        setTasks(tasks.filter(task => task.id !== id));
+        setTasks(tasks.filter(task => task.id !== id)); // we remove it by filter and the useState will be changed
     }
     
     return (
