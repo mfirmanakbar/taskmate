8a30641c35ea1410147f088a04105148582eb579
mfirmanakbar <em.firman.akbar@gmail.com>
2023-04-06 22:20:26 +0700

    adding use ref

--- src/components/AddTask-Backup.js
+++ src/components/AddTask-Backup.js
@@ -1,27 +1,24 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./AddTask.css";
 
 export const AddTask = ({tasks, setTasks}) => {
-    //const [taskValue, setTaskValue] = useState("");
+    const [taskValue, setTaskValue] = useState("");
     const [progress, setProgress] = useState(false);
-    const taskRef = useRef("");
 
-    // const handleChange = (event) => {
-    //     taskRef.current.valueOf = "";
-    // }
+    const handleChange = (event) => {
+        setTaskValue(event.target.value);
+    }
 
     const handleReset = () => {
-        //setTaskValue("");
+        setTaskValue("");
         setProgress(false)
-        taskRef.current.value = "";
     }
 
     const handleSubmit = (event) => {
         event.preventDefault(); // when onSubmit clicked, it won't refresh/reload the page
         const task = {
             id: Math.floor(Math.random() * 10000),
-            //name: taskValue,
-            name: taskRef.current.value,
+            name: taskValue,
             completed: Boolean(progress)
         };
         console.log(task);
@@ -33,8 +30,7 @@ export const AddTask = ({tasks, setTasks}) => {
         <section className="addtask">
             <form onSubmit={handleSubmit}>
                 {/* <input onChange={(e) => setTaskValue(e.target.value)} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" /> */}
-                {/* <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} /> */}
-                <input type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" ref={taskRef} />
+                <input onChange={handleChange} type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" value={taskValue} />
                 <select onChange={(event) => setProgress(event.target.value)} value={progress}>
                     <option value="false">Pending</option>
                     <option value="true">Completed</option>
@@ -42,10 +38,7 @@ export const AddTask = ({tasks, setTasks}) => {
                 <span onClick={handleReset} className="reset">Reset</span>
                 <button type="submit">Add Task</button>
             </form>
-            
-            {/* we can't useRef to render this UI.  */}
-            {/* <p>{taskValue} has {taskValue.length} length</p> */} 
-
+            <p>{taskValue} has {taskValue.length} length</p>
         </section>
     )
 }
